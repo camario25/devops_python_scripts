7a01656833442c63922d31ae57b2eed3d25c3711
camario25 <camario25@yahoo.com>
2019-05-12 21:24:27 -0700

    added reverse words script

---
+++ reverse_words.py
@@ -0,0 +1,17 @@
+def main():
+    def reverse_words():
+        user_words = input("Enter three words to reverse ")
+        user_arr = user_words.split()
+        rev_words = ' '.join(user_arr[::-1])
+        print("New order is {}".format(rev_words))
+    reverse_words()
+    quest = input("Would you like try more words? write 'yes' or 'no'")
+    if quest == 'no' or quest == 'n':
+        exit()
+    else:
+        user_words = input("Enter three words to reverse ")
+        user_arr = user_words.split()
+        rev_words = '.'.join(user_arr[::-1])
+        print("New order is {}".format(rev_words))
+
+main()
