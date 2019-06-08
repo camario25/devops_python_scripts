6250e2bbd3c50c0207817647e7641ea5464df719
camario25 <camario25@yahoo.com>
2019-06-08 10:14:11 -0700

    create ubuntu and windows ec2

---
+++ apache_exercise.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt install apache2
+wget -p -k --level=1 https://gameheadsoakland.org
+
--- create-ec2.py
+++ create-ec2.py
@@ -2,7 +2,12 @@ import boto3
 ec2 = boto3.resource('ec2')
 
 # create a new EC2 instance
-instances = ec2.create_instances(ImageId='ami-0a313d6098716f372', MinCount=1, MaxCount=1, InstanceType='t2.micro', KeyName='linux-sandbox')
+ubuntu_instances = ec2.create_instances(ImageId='ami-024a64a6685d05041', MinCount=1, MaxCount=1, InstanceType='t2.micro', KeyName='linux-sandbox')
 
-for instance in instances:
-  instance.create_tags(Tags=[{'Key':'Name', 'Value':'Mario_Alcantar'}, {'Key':'Server_Type', 'Value':'Webserver'}]) 
+for instance in ubuntu_instances:
+  instance.create_tags(Tags=[{'Key':'Name', 'Value':'Mario_A'}, {'Key':'Server_Type', 'Value':'Ubuntu-Webserver'}]) 
+  
+windows_instances = ec2.create_instances(ImageId='ami-0a9ca0496f746e6e0', MinCount=1, MaxCount=1, InstanceType='t2.micro', KeyName='linux-sandbox')
+
+for instance in windows_instances:
+  instance.create_tags(Tags=[{'Key':'Name', 'Value':'Mario_A'}, {'Key':'Server_Type', 'Value':'Windows-Webserver'}]) 
